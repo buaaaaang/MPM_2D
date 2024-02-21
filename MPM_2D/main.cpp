11fe030ca3ae69c8eeb52e1c88942e847e8b6939
buaaaaang <buaaaaang@kaist.ac.kr>
2024-02-21 16:32:33 +0900

    seperate shader file

--- MPM_2D/main.cpp
+++ MPM_2D/main.cpp
@@ -267,53 +267,10 @@ void Renderer::buildShaders()
 {
     using NS::StringEncoding::UTF8StringEncoding;
 
-    const char* shaderSrc = R"(
-        #include <metal_stdlib>
-        using namespace metal;
 
-        struct v2f
-        {
-            float4 position [[position]];
-            half3 color;
-        };
-
-        struct VertexData
-        {
-            float3 position;
-        };
-
-        struct InstanceData
-        {
-            float4x4 instanceTransform;
-            float4 instanceColor;
-        };
-
-        v2f vertex vertexMain( device const VertexData* vertexData [[buffer(0)]],
-                               device const InstanceData* instanceData [[buffer(1)]],
-                               uint vertexId [[vertex_id]],
-                               uint instanceId [[instance_id]] )
-        {
-            v2f o;
-            float4 pos = float4( vertexData[ vertexId ].position, 1.0 );
-            o.position = instanceData[ instanceId ].instanceTransform * pos;
-            o.color = half3( instanceData[ instanceId ].instanceColor.rgb );
-            return o;
-        }
-
-        half4 fragment fragmentMain( v2f in [[stage_in]] )
-        {
-            return half4( in.color, 1.0 );
-        }
-    )";
-
-    NS::Error* pError = nullptr;
+    
     MTL::Library* pLibrary = _pDevice->newDefaultLibrary();
     
-    if ( !pLibrary )
-    {
-        __builtin_printf( "%s", pError->localizedDescription()->utf8String() );
-        assert( false );
-    }
 
     MTL::Function* pVertexFn = pLibrary->newFunction( NS::String::string("vertexMain", UTF8StringEncoding) );
     MTL::Function* pFragFn = pLibrary->newFunction( NS::String::string("fragmentMain", UTF8StringEncoding) );
@@ -322,7 +279,7 @@ void Renderer::buildShaders()
     pDesc->setVertexFunction( pVertexFn );
     pDesc->setFragmentFunction( pFragFn );
     pDesc->colorAttachments()->object(0)->setPixelFormat( MTL::PixelFormat::PixelFormatBGRA8Unorm );
-
+    NS::Error* pError = nullptr;
     _pPSO = _pDevice->newRenderPipelineState( pDesc, &pError );
     if ( !_pPSO )
     {
